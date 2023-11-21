6f74bc60168559f3263a6282a7aa312b31303fec
Aniruddha Adak <adakaniruddha9@gmail.com>
2023-11-21 23:05:41 +0530

    day-56

---
+++ day56/script.js
@@ -0,0 +1,29 @@
+console.log("I am learniong conditionals in javascript from Code-with-harry");
+console.log("it is awesome ");
+
+// if-else if -else ladder
+let age =20;
+if(age >= 18 && age<75){
+    console.log("You can go anywhere you want");
+}
+
+else if(age >= 75 && age <=100){
+    console.log("You can not go anywhere without someone ");
+}
+
+else if(age <18){
+    console.log("You can go ,but you have to ask youe parent for permission");
+}
+
+else{
+    console.log("I don't know .Please enter your valid age ");
+}
+
+// single line comment in js
+/*
+multi
+line
+comment
+in 
+javascript
+ */
