ff5f4430c9b8ce12c07bf423248c38699ea30aaf
Aniruddha Adak <adakaniruddha9@gmail.com>
2023-11-24 20:24:08 +0530

    Add day59 and modify day 57

---
+++ day57/script.js
@@ -0,0 +1,17 @@
+console.log("I am tutorial on loops by code-with-harry");
+console.log("I am learning something new ");
+
+for (let i = 0; i < 40; i++) {
+  console.log(i + 1);
+}
+
+// object for forin loop
+let object = {
+  name: "Ani",
+  role: "Programmer",
+  love: "You",
+  roll: 30,
+};
+for (const key in object) {
+  console.log(key);
+}
